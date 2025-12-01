8fea8b93adfdf97b978bb55bdebb8849942748d3
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-01 15:48:27 -0500

    initial commit

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hi my name is ");
+        String name = "Slim Shady";
+        System.out.println("My Name Is Lyrics: Hi, my name is, what? My name is, who? / My name is, chka-chka, " + name);
     }
 }
